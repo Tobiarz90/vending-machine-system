677e9442cfe1c120d39b9e50679c9de3b55e5a73
Bartosz Tobiasz <barticz90@gmail.com>
2022-05-16 21:46:32 +0200

    ###

--- src/main/java/pl/coderslab/vendingmachinesystem/VendingMachineSystemApplication.java
+++ src/main/java/pl/coderslab/vendingmachinesystem/VendingMachineSystemApplication.java
@@ -2,12 +2,18 @@ package pl.coderslab.vendingmachinesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendingMachineSystemApplication {
+public class VendingMachineSystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VendingMachineSystemApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VendingMachineSystemApplication.class);
+    }
 }
--- src/main/java/pl/coderslab/vendingmachinesystem/controllers/UserPanelController.java
+++ src/main/java/pl/coderslab/vendingmachinesystem/controllers/UserPanelController.java
@@ -1,7 +1,27 @@
 package pl.coderslab.vendingmachinesystem.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.vendingmachinesystem.entities.StockItem;
+import pl.coderslab.vendingmachinesystem.repositories.StockItemRepository;
+
+import java.util.List;
 
 @Controller
+@RequestMapping(path = "/user")
 public class UserPanelController {
+
+    private final StockItemRepository stockItemRepository;
+
+    public UserPanelController(StockItemRepository stockItemRepository) {
+        this.stockItemRepository = stockItemRepository;
+    }
+
+    @GetMapping(path = "/machine")
+    public String showStockItems(Model model) {
+        List<StockItem> stockItems = stockItemRepository.findAll();
+        return "machine";
+    }
 }
