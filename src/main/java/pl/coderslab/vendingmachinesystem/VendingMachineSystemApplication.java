38e769e9373d068b95c67c68335c71772af9c1fa
Bartosz Tobiasz <barticz90@gmail.com>
2022-05-16 18:26:22 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/vendingmachinesystem/VendingMachineSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.vendingmachinesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendingMachineSystemApplication.class, args);
+    }
+
+}
