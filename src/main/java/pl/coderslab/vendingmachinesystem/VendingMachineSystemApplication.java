37a1dfc6b5fbdd661f33302d0eacabfbef75aae6
Bartosz Tobiasz <barticz90@gmail.com>
2022-05-18 18:09:32 +0200

    Revert '###' commit for VendingMachineSystemApplication class

--- src/main/java/pl/coderslab/vendingmachinesystem/VendingMachineSystemApplication.java
+++ src/main/java/pl/coderslab/vendingmachinesystem/VendingMachineSystemApplication.java
@@ -2,18 +2,13 @@ package pl.coderslab.vendingmachinesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendingMachineSystemApplication extends SpringBootServletInitializer {
+public class VendingMachineSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VendingMachineSystemApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(VendingMachineSystemApplication.class);
-    }
 }
