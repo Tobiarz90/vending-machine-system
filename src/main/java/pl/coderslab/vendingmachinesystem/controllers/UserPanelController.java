ae92210825c1084d69a88934dfffbfc14aef8b56
Bartosz Tobiasz <barticz90@gmail.com>
2022-05-19 18:21:16 +0200

    Add stock creation - model attribute value

--- src/main/java/pl/coderslab/vendingmachinesystem/controllers/UserPanelController.java
+++ src/main/java/pl/coderslab/vendingmachinesystem/controllers/UserPanelController.java
@@ -8,6 +8,8 @@ import pl.coderslab.vendingmachinesystem.entities.StockItem;
 import pl.coderslab.vendingmachinesystem.repositories.StockItemRepository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path = "/user")
@@ -20,8 +22,14 @@ public class UserPanelController {
     }
 
     @GetMapping(path = "/machine")
-    public String showStockItems(Model model) {
+    public String showStock(Model model) {
         List<StockItem> stockItems = stockItemRepository.findAll();
+
+        Map<Integer, Map<Integer, List<StockItem>>> stock = stockItems.stream()
+                .collect(Collectors.groupingBy(StockItem::getNoHorizontally,
+                        Collectors.groupingBy(StockItem::getNoVertically)));
+        model.addAttribute("stock", stock);
+
         return "machine";
     }
 }
