2be13fbb3f04e032035958144e0dbbba3259852c
Bartosz Tobiasz <barticz90@gmail.com>
2022-05-25 10:30:22 +0200

    Add Machine attributes to model and refactor for StockItemService bean inside showStock method

--- src/main/java/pl/coderslab/vendingmachinesystem/controllers/UserPanelController.java
+++ src/main/java/pl/coderslab/vendingmachinesystem/controllers/UserPanelController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.vendingmachinesystem.Machine;
 import pl.coderslab.vendingmachinesystem.entities.StockItem;
 import pl.coderslab.vendingmachinesystem.repositories.StockItemRepository;
+import pl.coderslab.vendingmachinesystem.services.StockItemService;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path = "/user")
@@ -17,18 +18,22 @@ public class UserPanelController {
 
     private final StockItemRepository stockItemRepository;
 
-    public UserPanelController(StockItemRepository stockItemRepository) {
+    private final StockItemService stockItemService;
+
+    public UserPanelController(StockItemRepository stockItemRepository, StockItemService stockItemService) {
         this.stockItemRepository = stockItemRepository;
+        this.stockItemService = stockItemService;
     }
 
     @GetMapping(path = "/machine")
     public String showStock(Model model) {
         List<StockItem> stockItems = stockItemRepository.findAll();
 
-        Map<Integer, Map<Integer, List<StockItem>>> stock = stockItems.stream()
-                .collect(Collectors.groupingBy(StockItem::getNoHorizontally,
-                        Collectors.groupingBy(StockItem::getNoVertically)));
+        Map<Integer, Map<Integer, List<StockItem>>> stock = stockItemService.getStock(stockItems);
         model.addAttribute("stock", stock);
+
+        model.addAttribute("rows", Machine.ROWS);
+        model.addAttribute("columns", Machine.COLUMNS);
 
         return "machine";
     }
