7fc586df0361c6d021fa89cbcaeca967bfe73581
Bartosz Tobiasz <barticz90@gmail.com>
2022-05-28 11:35:48 +0200

    Add basic Spring Data JPA and Spring Security annotations to the admin panel

--- src/main/java/pl/coderslab/vendingmachinesystem/controllers/AdminPanelController.java
+++ src/main/java/pl/coderslab/vendingmachinesystem/controllers/AdminPanelController.java
@@ -1,4 +1,11 @@
 package pl.coderslab.vendingmachinesystem.controllers;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/admin")
+@Secured("ROLE_ADMIN")
 public class AdminPanelController {
 }
